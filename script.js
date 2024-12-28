7ca2d0d4ea0dcd21d54bc54ba85d5db2f2cbe164
hemantkumarsuman <hemantkumar1699@gmail.com>
2024-12-29 03:33:17 +0530

    Dom Practice questions

---
+++ script.js
@@ -0,0 +1,32 @@
+// function changeContent(){
+//     let h1 = document.getElementById('origin');
+//     h1.textContent='changed text';
+// }
+
+
+// function changeColour(){
+//     let box=document.getElementById('box');
+//     box.style.backgroundColor='blue'
+// }
+
+
+// function toggleHighlight() {
+//     // Get the <p> element by its ID
+//     const paragraph = document.getElementById("text");
+
+//     // Add an event listener for the 'click' event
+//     paragraph.addEventListener("click", function () {
+//         // Toggle the "highlight" class on the element
+//         paragraph.classList.toggle("highlight");
+//     });
+// }
+
+// // Call the function to initialize the event listener
+// toggleHighlight();
+
+// highlight();
+
+
+
+
+
