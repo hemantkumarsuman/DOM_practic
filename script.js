d20f69fde91a4fa1c5164f74dee003cabf5d8f06
hemantkumarsuman <hemantkumar1699@gmail.com>
2025-01-25 18:08:50 +0530

    Write a function filterRange(arr, a, b) that gets an array arr, looks for elements with values higher or equal to a and lower or equal to b and return a result as an array.

--- script.js
+++ script.js
@@ -422,6 +422,21 @@
 
 
 
+// Write a function filterRange(arr, a, b) that gets an array arr, looks for elements with values higher or equal to a
+// and lower or equal to b and return a result as an array.
+
+function filterRange(arr,a,b){
+    return arr.filter((item)=>(item>=a && item<=b))
+}
+let arr = [5, 3, 8, 1];
+
+let filtered = filterRange(arr, 1, 4);
+
+console.log(filtered);// 3,1 (matching values)
+
+console.log( arr ); // 5,3,8,1 (not modified)
+
+
 
 
 
